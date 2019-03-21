6180198c90c2c8ec478e5a27f649303e888bcbaf
nsilvestre@belatrixsf.com <nsilvestre@belatrixsf.com>
2019-03-21 17:59:44 -0300

    changing app to point to ToDoList project location

--- src/test/java/SimulatorTest.java
+++ src/test/java/SimulatorTest.java
@@ -18,8 +18,11 @@ public class SimulatorTest {
         capabilities.setCapability("platformName", "iOS");
         capabilities.setCapability("platformVersion", "12.1");
         capabilities.setCapability("deviceName", "iPad Air 2");
-      //  capabilities.setCapability("bundleId", "com.apple.reminders");
-        capabilities.setCapability("app", "/Users/travis/build/nsilvestre/appiumTest/ToDoList/builddata/Build/Products/Release-iphonesimulator/ToDoList.app");
+        //  capabilities.setCapability("bundleId", "com.apple.reminders");
+        /* Below capability must be uncommented for /AppiumTest job */
+         //  capabilities.setCapability("app", "/Users/travis/build/nsilvestre/appiumTest/ToDoList/builddata/Build/Products/Release-iphonesimulator/ToDoList.app");
+        /* Below capability must be uncommented for /ToDoList job */
+        capabilities.setCapability("app", "/Users/travis/build/nsilvestre/ToDoList/builddata/Build/Products/Release-iphonesimulator/ToDoList.app");
         capabilities.setCapability("noReset", "true");
         capabilities.setCapability("useNewWDA", "false");
 
